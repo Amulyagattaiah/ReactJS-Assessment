21e4a692620021a1957a4bc5d3433b11221e4003
Amulyagattaiah <amulyagattaiah@gmail.com>
2025-01-22 07:55:24 +0530

    Task-1

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
 import { List } from 'ui'
 
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
 const App = () => {
+  const [pokemonList, setPokemonList] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const response = await fetch(api);
+        const data = await response.json();
+        const pokemonNames = data.results.map((pokemon: { name: string }) => pokemon.name);
+        setPokemonList(pokemonNames);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
   return (
   <>
     <h1>Pokemon list:</h1>
