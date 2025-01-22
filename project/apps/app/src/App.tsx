61cb327c672059597873cfefa0a3d089579ed856
Krishna <krishkoraganji023@gmail.com>
2025-01-22 09:03:44 +0530

    task-2

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,16 +1,18 @@
-import { useEffect, useState } from 'react';
-import { List } from 'ui'
+import { useEffect, useState } from "react";
+import { List } from "ui";
+import { Pokemon } from "ui/components/List";
 
-const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
+const api = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
 const App = () => {
-  const [pokemonList, setPokemonList] = useState<string[]>([]);
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
   useEffect(() => {
     const fetchPokemon = async () => {
       try {
         const response = await fetch(api);
         const data = await response.json();
-        const pokemonNames = data.results.map((pokemon: { name: string }) => pokemon.name);
+        console.log("data", data);
+        const pokemonNames = data.results;
         setPokemonList(pokemonNames);
       } catch (error) {
         console.error(error);
@@ -20,11 +22,11 @@ const App = () => {
     fetchPokemon();
   }, []);
   return (
-  <>
-    <h1>Pokemon list:</h1>
-    <List />
-  </>
-  )
-}
+    <>
+      <h1>Pokemon list:</h1>
+      <List pokemonList={pokemonList} />
+    </>
+  );
+};
 
-export default App
+export default App;
--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
-
-export const List = () => {
-  return <p>List</p>
+export interface Pokemon {
+  name: string;
+  url: string;
 }
+[];
+
+interface ListProps {
+  pokemonList: Pokemon[];
+}
+
+const List: React.FC<ListProps> = ({ pokemonList }) => {
+  return (
+    <ul>
+      {pokemonList.map((pokemon, index) => (
+        <li key={index}>{pokemon.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default List;
